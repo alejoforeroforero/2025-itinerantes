8d388754cf5c2e6dad9f61e1bb23e3daa5211038
alejoforeroforero <alejoforeroforero@gmail.com>
2025-06-03 13:50:51 -0500

    itinerantes v1

--- src/app/sobre-nosotros/page.tsx
+++ src/app/sobre-nosotros/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { About } from "@/components/";
 
 export default function AboutPage() {
--- src/components/website/about/text-editor/plugins/ImagesPlugin/index.tsx
+++ src/components/website/about/text-editor/plugins/ImagesPlugin/index.tsx
@@ -297,37 +297,6 @@ export default function ImagesPlugin({
 
 const TRANSPARENT_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
-function $onDragStart(event: DragEvent): boolean {
-  const node = $getImageNodeInSelection();
-  if (!node) {
-    return false;
-  }
-  const dataTransfer = event.dataTransfer;
-  if (!dataTransfer) {
-    return false;
-  }
-  dataTransfer.setData('text/plain', '_');
-  dataTransfer.setDragImage(img, 0, 0);
-  dataTransfer.setData(
-    'application/x-lexical-drag',
-    JSON.stringify({
-      data: {
-        altText: node.__altText,
-        caption: node.__caption,
-        height: node.__height,
-        key: node.getKey(),
-        maxWidth: node.__maxWidth,
-        showCaption: node.__showCaption,
-        src: node.__src,
-        width: node.__width,
-      },
-      type: 'image',
-    }),
-  );
-
-  return true;
-}
-
 function $onDragover(event: DragEvent): boolean {
   const node = $getImageNodeInSelection();
   if (!node) {
